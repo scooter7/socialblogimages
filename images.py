8c1e6f469cd48fc331d3e0479f1064c34c64fe74
scooter7 <scooter.vineburgh@gmail.com>
2023-11-04 22:43:53 -0500

    Update images.py

--- images.py
+++ images.py
@@ -6,6 +6,10 @@ from io import BytesIO
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import keras_ocr
+import cv2
+import numpy as np
+import math
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -18,8 +22,28 @@ def extract_keywords(text):
     filtered_text = [word for word in word_tokens if not word.lower() in stop_words]
     return ' '.join(filtered_text)
 
+def midpoint(x1, y1, x2, y2):
+    x_mid = int((x1 + x2)/2)
+    y_mid = int((y1 + y2)/2)
+    return (x_mid, y_mid)
+
+def inpaint_text(image, pipeline):
+    prediction_groups = pipeline.recognize([image])
+    mask = np.zeros(image.shape[:2], dtype="uint8")
+    for box in prediction_groups[0]:
+        x0, y0 = box[1][0]
+        x1, y1 = box[1][1] 
+        x2, y2 = box[1][2]
+        x3, y3 = box[1][3]
+        x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
+        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
+        thickness = int(math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
+        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255, thickness)
+    inpainted_img = cv2.inpaint(image, mask, 7, cv2.INPAINT_NS)
+    return inpainted_img
+
 def generate_image(prompt, width, height):
-    prompt += " | only picture"
+    prompt += " | only picture, no text"
     response = openai.Image.create(
         prompt=prompt,
         n=1,
@@ -29,6 +53,8 @@ def generate_image(prompt, width, height):
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
     return image
+
+pipeline = keras_ocr.pipeline.Pipeline()
 
 st.title('Text to Image Generator')
 
@@ -40,4 +66,7 @@ user_input = st.text_area("Enter your text here:")
 if st.button('Generate Image'):
     keywords = extract_keywords(user_input)
     image = generate_image(keywords, width, height)
-    st.image(image, caption='Generated Image')
+    image_np = np.array(image)
+    image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+    img_text_removed = inpaint_text(image_np, pipeline)
+    st.image(img_text_removed, caption='Generated Image', channels="BGR")
